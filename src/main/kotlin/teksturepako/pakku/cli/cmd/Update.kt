95d89db0fdd86f901b0c8a4a1762ab332a61891b
Juraj Hrivnak <bass.jurajhrivnak@gmail.com>
2024-01-18 12:19:59 +0100

    Refactor `update` command

--- src/main/kotlin/teksturepako/pakku/cli/cmd/Update.kt
+++ src/main/kotlin/teksturepako/pakku/cli/cmd/Update.kt
@@ -6,19 +6,15 @@ import com.github.ajalt.clikt.parameters.arguments.argument
 import com.github.ajalt.clikt.parameters.arguments.multiple
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
-import kotlinx.coroutines.*
-import kotlinx.coroutines.channels.ReceiveChannel
-import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.runBlocking
 import teksturepako.pakku.api.data.PakkuLock
 import teksturepako.pakku.api.platforms.Multiplatform
-import teksturepako.pakku.api.projects.Project
 
 class Update : CliktCommand("Update projects")
 {
-    private val projectArgs: List<String> by argument().multiple()
+    private val projectArgs: List<String> by argument("projects").multiple()
     private val allFlag: Boolean by option("-a", "--all", help = "Update all projects").flag()
 
-    @OptIn(ExperimentalCoroutinesApi::class, DelicateCoroutinesApi::class)
     override fun run() = runBlocking {
         val pakkuLock = PakkuLock.readToResult().fold(
             onSuccess = { it },
@@ -32,7 +28,8 @@ class Update : CliktCommand("Update projects")
         val oldProjects = if (allFlag)
         {
             pakkuLock.getAllProjects()
-        } else
+        }
+        else
         {
             projectArgs.mapNotNull { projectArg ->
                 pakkuLock.getProject(projectArg).also {
@@ -44,87 +41,23 @@ class Update : CliktCommand("Update projects")
             }
         }
 
-        val updatedProjects: ReceiveChannel<Project> = produce {
-            val oldProjectIds = oldProjects.asSequence()
-                .flatMap {
-                    it.id.asSequence()
-                }.groupBy({ it.key }, { it.value })
-
-            val newProjects = Multiplatform.requestMultipleProjectsWithFiles(
-                pakkuLock.getMcVersions(), pakkuLock.getLoaders(), oldProjectIds, numberOfFiles = 1
-            )
-
-            oldProjects.map { oldProject ->
-                launch {
-                    var updatedProject = oldProject.copy(files = mutableSetOf())
-
-                    // Handle versions
-                    val mcVersions = oldProject.files.flatMap { it.mcVersions }
-                    val loaders = oldProject.files.flatMap { it.loaders }
-
-                    newProjects.forEach { newProject ->
-                        if (newProject isAlmostTheSameAs oldProject)
-                        {
-                            updatedProject += newProject
-
-                            if (updatedProject != oldProject && mcVersions.isNotEmpty() && loaders.isNotEmpty())
-                            {
-                                send(updatedProject) // Send updated project to channel
-                            }
-                        }
-                    }
-                }
-            }.joinAll() // Wait for all jobs to finish
-            close() // Close channel
-
-//            oldProjects.map { oldProject ->
-//                launch {
-//                    /** Create copy of each project with empty files */
-//                    var updatedProject = oldProject.copy(files = mutableSetOf())
-//
-//                    val mcVersions = oldProject.files.flatMap { it.mcVersions }
-//                    val loaders = oldProject.files.flatMap { it.loaders }
-//
-//                    /** For each platform request a new project with files and combine it with [updated project] */
-//                    x@for (platform in Multiplatform.platforms)
-//                    {
-//                        val projectId = oldProject.id[platform.serialName] ?: continue@x
-//
-//                        platform.requestProjectWithFiles(mcVersions, loaders, projectId)
-//                            ?.let { updatedProject += it } // Combine projects
-//                    }
-//
-//                    /** If updated project is different from old project return updated project */
-//                    if (updatedProject != oldProject && mcVersions.isNotEmpty() && loaders.isNotEmpty())
-//                    {
-//                        send(updatedProject) // Send updated project to channel
-//                    }
-//                }
-//            }.joinAll() // Wait for all jobs to finish
-//            close() // Close channel
-        }
-
-        var updated = false
-        val jobs = mutableListOf<Job>()
+        val updatedProjects = Multiplatform.updateMultipleProjectsWithFiles(
+            pakkuLock.getMcVersions(), pakkuLock.getLoaders(), oldProjects.toMutableSet(), numberOfFiles = 1
+        )
 
         for (updatedProject in updatedProjects)
         {
-            jobs += launch {
-                pakkuLock.update(updatedProject)
-                terminal.success("${updatedProject.slug} updated")
-
-                updated = true
-            }
+            pakkuLock.update(updatedProject)
+            terminal.success("${updatedProject.slug} updated")
         }
-        jobs.joinAll()
 
-        if (!updated && updatedProjects.isClosedForReceive && oldProjects.isNotEmpty())
+        if (updatedProjects.isEmpty())
         {
             when
             {
                 allFlag || projectArgs.isEmpty() -> terminal.success("All projects are up to date")
                 oldProjects.size == 1            -> terminal.success("${oldProjects.first().slug} is up to date")
-                else                         ->
+                else                             ->
                 {
                     terminal.success("${oldProjects.map { it.slug }} are up to date")
                 }
