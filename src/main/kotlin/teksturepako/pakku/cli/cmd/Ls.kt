b303b4f54f27ec3611e5d15b42769fabd94f4049
Juraj Hrivnak <bass.jurajhrivnak@gmail.com>
2024-01-26 01:14:21 +0100

    Redesign `ls` command & add hyperlinks for projects

--- src/main/kotlin/teksturepako/pakku/cli/cmd/Ls.kt
+++ src/main/kotlin/teksturepako/pakku/cli/cmd/Ls.kt
@@ -2,34 +2,87 @@ package teksturepako.pakku.cli.cmd
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.terminal
+import com.github.ajalt.mordant.rendering.TextColors.*
+import com.github.ajalt.mordant.rendering.TextStyle
 import kotlinx.coroutines.runBlocking
 import teksturepako.pakku.api.data.PakkuLock
 import teksturepako.pakku.api.platforms.Multiplatform
+import teksturepako.pakku.api.platforms.Platform
+import teksturepako.pakku.api.projects.UpdateStrategy
+import teksturepako.pakku.api.projects.containsProject
+
 
 class Ls : CliktCommand("List projects")
 {
     override fun run() = runBlocking {
-        val pakkuLock = PakkuLock.readToResult().fold(
-            onSuccess = { it },
-            onFailure = {
-                terminal.danger(it.message)
-                echo()
-                return@runBlocking
-            }
-        )
+        val pakkuLock = PakkuLock.readToResult().getOrElse {
+            terminal.danger(it.message)
+            echo()
+            return@runBlocking
+        }
 
         val projects = pakkuLock.getAllProjects()
+        val platforms: List<Platform> = pakkuLock.getPlatforms().getOrDefault(listOf())
+
+        val updatedProjects = Multiplatform.updateMultipleProjectsWithFiles(
+            pakkuLock.getMcVersions(), pakkuLock.getLoaders(), projects.toMutableSet(), numberOfFiles = 1
+        )
 
         for (project in projects)
         {
-            val name: String? = project.name.values.firstOrNull()
-            val links: String = project.pakkuLinks.size.toString() + "\uD83D\uDD17"
-            val platforms: String = Multiplatform.platforms.joinToString(" ") {
-                if (project.hasFilesOnPlatform(it)) "${it.name}✔\uFE0F" else "${it.name}❌"
+            val upds = when (project.updateStrategy)
+            {
+                UpdateStrategy.LATEST ->
+                {
+                    if (updatedProjects containsProject project)
+                    {
+                        blue(project.updateStrategy.short)
+                    }
+                    else brightGreen(project.updateStrategy.short)
+                }
+                UpdateStrategy.NONE   -> red(project.updateStrategy.short)
+                else                  -> cyan(project.updateStrategy.short)
             }
 
-            terminal.success("($links $platforms) $name")
+            val name: String? = when
+            {
+                project.canRedistribute ->
+                {
+                    if (project.hasNoFiles()) project.name.values.firstOrNull()?.let { red(it) }
+                    else project.name.values.firstOrNull()
+                }
+                else                    ->
+                {
+                    if (project.hasNoFiles()) project.name.values.firstOrNull()?.let {
+                        TextStyle(bgColor = gray, color = red)("⚠$it")
+                    } else project.name.values.firstOrNull()?.let {
+                        TextStyle(bgColor = gray, color = black)("⚠$it")
+                    }
+                }
+            }
+
+            val deps: String = when
+            {
+                project.pakkuLinks.size > 1  -> "${project.pakkuLinks.size} deps"
+                project.pakkuLinks.size == 1 -> "1 dep "
+                else                         -> "      "
+            }
+
+            val targets: String = platforms.joinToString(" ") {
+                if (project.hasFilesOnPlatform(it))
+                {
+                    TextStyle(
+                        color = brightGreen,
+                        hyperlink = "${it.url}/${project.slug[it.serialName]}"
+                    )(it.shortName)
+                }
+                else red(it.shortName)
+            }
+
+            terminal.println(" $deps | $targets | $upds$name")
         }
+        echo()
         terminal.info("Projects total: ${projects.size}")
+        echo()
     }
 }
