3d435e5480101c8764d7e917acc61fa2c4d9ecd0
Juraj Hrivnak <bass.jurajhrivnak@gmail.com>
2024-01-26 01:12:06 +0100

    Implement `target` configuration for `add` command & link projects to its already added dependants

--- src/main/kotlin/teksturepako/pakku/cli/cmd/Add.kt
+++ src/main/kotlin/teksturepako/pakku/cli/cmd/Add.kt
@@ -8,7 +8,6 @@ import com.github.ajalt.mordant.terminal.YesNoPrompt
 import kotlinx.coroutines.runBlocking
 import teksturepako.pakku.api.actions.createAdditionRequest
 import teksturepako.pakku.api.data.PakkuLock
-import teksturepako.pakku.api.platforms.Multiplatform
 import teksturepako.pakku.api.platforms.Platform
 import teksturepako.pakku.cli.promptForProject
 import teksturepako.pakku.cli.resolveDependencies
@@ -18,33 +17,44 @@ class Add : CliktCommand("Add projects")
     private val projectArgs: List<String> by argument("projects").multiple(required = true)
 
     override fun run() = runBlocking {
-        val pakkuLock = PakkuLock.readToResult().fold(
-            onSuccess = { it },
-            onFailure = {
-                terminal.danger(it.message)
-                echo()
-                return@runBlocking
-            }
-        )
+        val pakkuLock = PakkuLock.readToResult().getOrElse {
+            terminal.danger(it.message)
+            echo()
+            return@runBlocking
+        }
 
-        // TODO: Configuration
-        val platforms: List<Platform> = Multiplatform.platforms
-        val projectProvider = Multiplatform
+        // Configuration
+        val platforms: List<Platform> = pakkuLock.getPlatforms().getOrElse {
+            terminal.danger(it.message)
+            echo()
+            return@runBlocking
+        }
+
+        val projectProvider = pakkuLock.getProjectProvider().getOrElse {
+            terminal.danger(it.message)
+            echo()
+            return@runBlocking
+        }
         // --
 
         for (projectIn in projectArgs.map { arg ->
-            Multiplatform.requestProjectWithFiles(pakkuLock.getMcVersions(), pakkuLock.getLoaders(), arg)
+            projectProvider.requestProjectWithFiles(pakkuLock.getMcVersions(), pakkuLock.getLoaders(), arg)
         })
         {
             projectIn.createAdditionRequest(
-                onError = { error -> terminal.danger(error.message) },
+                onError = {
+                    error -> terminal.danger(error.message)
+                    echo()
+                },
                 onRetry = { platform -> promptForProject(platform, terminal, pakkuLock) },
                 onSuccess = { project, isRecommended, reqHandlers ->
                     if (YesNoPrompt("Do you want to add ${project.slug}?", terminal, isRecommended).ask() == true)
                     {
                         pakkuLock.add(project)
+                        pakkuLock.linkProjectToDependants(project)
                         project.resolveDependencies(terminal, reqHandlers, pakkuLock, projectProvider, platforms)
                         terminal.success("${project.slug} added")
+                        echo()
                     }
                 },
                 pakkuLock,
