c375f955f4888d7fe3fed322d2fa98f436aa00d7
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2024-06-05 16:54:56 +0200

    Implement auto generation for cli reference docs

--- src/commonMain/kotlin/teksturepako/pakku/cli/cmd/Pakku.kt
+++ src/commonMain/kotlin/teksturepako/pakku/cli/cmd/Pakku.kt
@@ -2,19 +2,64 @@ package teksturepako.pakku.cli.cmd
 
 import com.github.ajalt.clikt.completion.completionOption
 import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.parameters.options.flag
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.versionOption
+import com.github.ajalt.clikt.parameters.options.*
 import teksturepako.pakku.VERSION
 import teksturepako.pakku.api.data.workingPath
+import teksturepako.pakku.api.overrides.Overrides.PAKKU_DIR
 import teksturepako.pakku.cli.ui.overrideYes
 import teksturepako.pakku.debugMode
+import java.io.File
 
 class Pakku : CliktCommand()
 {
     init {
         versionOption(VERSION, help = "Get pakku version")
         completionOption(help = "Generate autocompletion scripts")
+        eagerOption("--generate-docs", help = "Generate WriterSide docs for CLI commands", hidden = true) {
+            val outputFile = File("$workingPath/$PAKKU_DIR/docs")
+            outputFile.mkdirs()
+
+            this@Pakku.registeredSubcommands().forEach { subcmd ->
+                val cmdFileName = "${this@Pakku.commandName}-${subcmd.commandName}.md"
+
+                val cmdName = subcmd.commandName
+                val cmdHelp = subcmd.commandHelp(this.context)
+
+                val text = buildString {
+                    append("# pakku $cmdName\n")
+                    append("\n")
+                    append("$cmdHelp\n")
+                    append("\n")
+                    append("## Usage\n")
+                    append("\n")
+                    append("<snippet id=\"snippet-cmd\">\n")
+                    append("    <var name=\"cmd\">$cmdName</var>\n")
+                    append("    <var name=\"help\"></var>\n")
+                    append("    <include from=\"_template_cmd.md\" element-id=\"template-cmd\"/>\n")
+                    append("</snippet>\n")
+                    append("\n")
+                    append("## Options\n")
+                    append("\n")
+                    append("<snippet id=\"snippet-options\">\n")
+                    append("\n")
+                    for (opt in subcmd.registeredOptions())
+                    {
+                        val optName = opt.names.joinToString { "`$it`" }
+                        val optHelp = opt.optionHelp(this@eagerOption.context)
+
+                        append("$optName\n")
+                        append(": $optHelp\n")
+                        append("\n")
+                    }
+                    append("</snippet>\n")
+                    append("")
+                }
+
+                val cmdFile = File(outputFile, cmdFileName)
+                cmdFile.createNewFile()
+                cmdFile.writeText(text)
+            }
+        }
     }
 
     private val yesFlag: Boolean by option(
