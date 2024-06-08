1a273eec1ac1b44ad0b6dddd006ffa70760c2226
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2024-06-08 22:00:49 +0200

    Increase request timeouts to avoid issues with big modpacks

--- src/jvmMain/kotlin/teksturepako/pakku/api/http/HttpClient.jvm.kt
+++ src/jvmMain/kotlin/teksturepako/pakku/api/http/HttpClient.jvm.kt
@@ -5,6 +5,8 @@ import io.ktor.client.engine.okhttp.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import kotlinx.serialization.json.Json
+import kotlin.time.Duration.Companion.minutes
+import kotlin.time.toJavaDuration
 
 @Suppress("DEPRECATION")
 actual val client = HttpClient(OkHttp) {
@@ -12,7 +14,9 @@ actual val client = HttpClient(OkHttp) {
         Json
     }
     install(HttpTimeout) {
-        socketTimeoutMillis = 600000
+        socketTimeoutMillis = 3.minutes.inWholeMilliseconds
+        requestTimeoutMillis = 3.minutes.inWholeMilliseconds
+        connectTimeoutMillis = 3.minutes.inWholeMilliseconds
     }
     install(UserAgent) {
         agent = "juraj-hrivnak/Pakku (tekeksturepako@gmail.com)"
@@ -22,6 +26,10 @@ actual val client = HttpClient(OkHttp) {
         pipelining = true
         config {
             retryOnConnectionFailure(true)
+
+            connectTimeout(3.minutes.toJavaDuration())
+            callTimeout(3.minutes.toJavaDuration())
+            writeTimeout(3.minutes.toJavaDuration())
         }
     }
 }
