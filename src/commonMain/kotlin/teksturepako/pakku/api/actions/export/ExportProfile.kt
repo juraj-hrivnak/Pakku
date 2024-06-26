49f06788dca86ea876d7d70639a7b4a2ec82f3a2
Juraj Hrivnak <bass.jurajhrivnak@gmail.com>
2024-06-26 19:41:55 +0200

    Add KDocs for export rule & profile

--- src/commonMain/kotlin/teksturepako/pakku/api/actions/export/ExportProfile.kt
+++ src/commonMain/kotlin/teksturepako/pakku/api/actions/export/ExportProfile.kt
@@ -1,8 +1,12 @@
 package teksturepako.pakku.api.actions.export
 
-import teksturepako.pakku.api.actions.export.rules.ExportRule
-
-data class ExportProfile(
+/**
+ * An export profile is used to contain a list of [export rules][ExportRule].
+ *
+ * Each export profile is independent of each other and
+ * will result in one exported file.
+ */
+open class ExportProfile(
     val name: String,
     val fileExtension: String = "zip",
     val rules: List<ExportRule?>
---
+++ src/commonMain/kotlin/teksturepako/pakku/api/actions/export/ExportRule.kt
@@ -0,0 +1,27 @@
+package teksturepako.pakku.api.actions.export
+
+/**
+ * A functional interface used to declare rules which control
+ * what should happen with the content you want to export/package.
+ *
+ * These rules can be written in many ways and are fully extensible
+ * by extending the [rule context][RuleContext].
+ *
+ * To create an export rule, you simply implement the functional interface like this:
+ *
+ * ```kotlin
+ * fun exampleRule() = ExportRule {
+ *     when (it)
+ *     {
+ *         is ExportingProject -> it.export() // (Sample function)
+ *         else                -> it.ignore()
+ *     }
+ * }
+ * ```
+ *
+ * Export rules must be part of an [export profile][ExportProfile] to be executed.
+ */
+fun interface ExportRule
+{
+    suspend fun getResult(ruleContext: RuleContext): RuleResult
+}
