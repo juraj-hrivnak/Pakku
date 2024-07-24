b2c5444c1d19ba8e9f15135c1c01d41c6b8f834e
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2024-07-24 17:46:47 +0200

    Implement docs auto gen. of cmd params & args

--- src/commonMain/kotlin/teksturepako/pakku/cli/cmd/Pakku.kt
+++ src/commonMain/kotlin/teksturepako/pakku/cli/cmd/Pakku.kt
@@ -2,13 +2,19 @@ package teksturepako.pakku.cli.cmd
 
 import com.github.ajalt.clikt.completion.completionOption
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.terminal
+import com.github.ajalt.clikt.output.HelpFormatter.ParameterHelp.Argument
 import com.github.ajalt.clikt.parameters.options.*
+import kotlinx.coroutines.runBlocking
 import teksturepako.pakku.VERSION
 import teksturepako.pakku.api.data.Dirs.PAKKU_DIR
 import teksturepako.pakku.api.data.workingPath
 import teksturepako.pakku.cli.ui.overrideYes
+import teksturepako.pakku.cli.ui.pInfo
 import teksturepako.pakku.debugMode
-import java.io.File
+import teksturepako.pakku.io.tryOrNull
+import kotlin.io.path.*
+import kotlin.system.exitProcess
 
 class Pakku : CliktCommand()
 {
@@ -18,7 +24,7 @@ class Pakku : CliktCommand()
         eagerOption(
             "--generate-docs", help = "Generate WriterSide docs for CLI commands", hidden = true
         ) {
-            generateDocs(this)
+            runBlocking { generateDocs(this@eagerOption) }
         }
     }
 
@@ -43,16 +49,38 @@ class Pakku : CliktCommand()
     }
 }
 
-fun CliktCommand.generateDocs(context: OptionTransformContext)
+@OptIn(ExperimentalPathApi::class)
+suspend fun CliktCommand.generateDocs(context: OptionTransformContext)
 {
-    val outputFile = File("$workingPath/$PAKKU_DIR/docs")
-    outputFile.mkdirs()
+    val outputFile = Path(workingPath, PAKKU_DIR, "docs")
+
+    outputFile.tryOrNull {
+        it.deleteRecursively()
+        it.createParentDirectories()
+        it.createDirectory()
+    }
+
+    terminal.pInfo("Generating docs:")
 
     this.registeredSubcommands().forEach { subcmd ->
         val cmdFileName = "${this.commandName}-${subcmd.commandName}.md"
 
         val cmdName = subcmd.commandName
         val cmdHelp = subcmd.commandHelp(context.context)
+
+        val args: List<Argument> = subcmd.allHelpParams().filterIsInstance<Argument>()
+
+        val cmdParams = "[&lt;options&gt;] " + args.joinToString(" ") { arg ->
+            buildString {
+                if (!arg.required) append("[")
+                append("&lt;${arg.name.lowercase()}&gt;")
+                if (!arg.required) append("]")
+                if (arg.repeatable) append("...")
+            }
+        }
+
+        val opts = subcmd.registeredOptions().filter { "--help" !in it.names }
+        val helpOpt = subcmd.registeredOptions().find { "--help" in it.names }
 
         val text = buildString {
             append("# pakku $cmdName\n")
@@ -64,17 +92,57 @@ fun CliktCommand.generateDocs(context: OptionTransformContext)
             append("<snippet id=\"snippet-cmd\">\n")
             append("\n")
             append("<var name=\"cmd\">$cmdName</var>\n")
-            append("<var name=\"help\"></var>\n")
+            append("<var name=\"params\">$cmdParams</var>\n")
             append("<include from=\"_template_cmd.md\" element-id=\"template-cmd\"/>\n")
             append("\n")
             append("</snippet>\n")
             append("\n")
+            if (args.isNotEmpty())
+            {
+                append("## Arguments\n")
+                append("\n")
+                append("<snippet id=\"snippet-args\">\n")
+                append("\n")
+                for (arg in args)
+                {
+                    val argName = buildString {
+                        append("`")
+                        if (!arg.required) append("[")
+                        append("<${arg.name.lowercase()}>")
+                        if (!arg.required) append("]")
+                        if (arg.repeatable) append("...")
+                        append("`")
+                    }
+                    val argHelp = arg.help.ifBlank { "The `${arg.name.lowercase()}` argument" }
+
+                    append("$argName\n")
+                    append(": $argHelp\n")
+                    append("\n")
+                }
+                append("</snippet>\n")
+                append("\n")
+            }
             append("## Options\n")
             append("\n")
-            append("<snippet id=\"snippet-options\">\n")
+            append("<snippet id=\"snippet-options-all\">\n")
             append("\n")
-            for (opt in subcmd.registeredOptions())
+            if (opts.isNotEmpty())
             {
+                append("<snippet id=\"snippet-options\">\n")
+                append("\n")
+                for (opt in opts)
+                {
+                    val optName = opt.names.joinToString { "`$it`" }
+                    val optHelp = opt.optionHelp(context.context)
+
+                    append("$optName\n")
+                    append(": $optHelp\n")
+                    append("\n")
+                }
+                append("</snippet>\n")
+                append("\n")
+            }
+            helpOpt?.let { opt ->
                 val optName = opt.names.joinToString { "`$it`" }
                 val optHelp = opt.optionHelp(context.context)
 
@@ -86,8 +154,12 @@ fun CliktCommand.generateDocs(context: OptionTransformContext)
             append("")
         }
 
-        val cmdFile = File(outputFile, cmdFileName)
-        cmdFile.createNewFile()
+        val cmdFile = Path(outputFile.pathString, cmdFileName)
+        cmdFile.createFile()
         cmdFile.writeText(text)
+
+        terminal.pInfo(cmdFile.pathString)
     }
+
+    exitProcess(0)
 }
