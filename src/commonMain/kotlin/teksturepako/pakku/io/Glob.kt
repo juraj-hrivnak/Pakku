64ed7755e69326d90e84b96524b62e2828756c8d
Juraj Hrivnak <bass.jurajhrivnak@gmail.com>
2024-10-10 15:58:26 +0200

    Implement negating "`!`" glob pattern

--- src/commonMain/kotlin/teksturepako/pakku/io/Glob.kt
+++ src/commonMain/kotlin/teksturepako/pakku/io/Glob.kt
@@ -1,5 +1,6 @@
 package teksturepako.pakku.io
 
+import teksturepako.pakku.debug
 import java.nio.file.FileSystems
 import java.nio.file.Path
 import kotlin.io.path.ExperimentalPathApi
@@ -8,12 +9,19 @@ import kotlin.io.path.PathWalkOption
 import kotlin.io.path.walk
 
 @OptIn(ExperimentalPathApi::class)
-fun Path.listDirectoryEntriesRecursive(glob: String): List<Path>
+fun Path.listDirectoryEntriesRecursive(glob: String): Sequence<Path>
 {
     val matcher = FileSystems.getDefault().getPathMatcher("glob:${glob.removePrefix("./")}")
-    return walk(PathWalkOption.INCLUDE_DIRECTORIES).filter { matcher.matches(it) }.toList()
+    return walk(PathWalkOption.INCLUDE_DIRECTORIES).filter { matcher.matches(it) }
 }
 
-fun List<String>.expandWithGlob() = flatMap { glob ->
-    Path("").listDirectoryEntriesRecursive(glob).map { it.toString() }
-}
+fun List<String>.expandWithGlob() = fold(listOf<String>()) { acc, glob ->
+    if (glob.startsWith("!"))
+    {
+        acc - Path("").listDirectoryEntriesRecursive(glob.removePrefix("!")).map { it.toString() }.toSet()
+    }
+    else
+    {
+        acc + Path("").listDirectoryEntriesRecursive(glob).map { it.toString() }
+    }
+}.debug(::println)
