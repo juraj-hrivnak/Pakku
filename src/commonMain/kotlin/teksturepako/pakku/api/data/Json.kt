4c49779b0e2b7f9d240b9ff4343df653e12bace9
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2024-12-22 17:07:00 +0100

    Allow trailing commas in config file & lock file

--- src/commonMain/kotlin/teksturepako/pakku/api/data/Json.kt
+++ src/commonMain/kotlin/teksturepako/pakku/api/data/Json.kt
@@ -1,13 +1,16 @@
 package teksturepako.pakku.api.data
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 
+@OptIn(ExperimentalSerializationApi::class)
 val json = Json {
     prettyPrint = true
     isLenient = true
     ignoreUnknownKeys = true
     classDiscriminator = "_internal"
     explicitNulls = false
+    allowTrailingComma = true
 }
 
 val jsonEncodeDefaults = Json {
