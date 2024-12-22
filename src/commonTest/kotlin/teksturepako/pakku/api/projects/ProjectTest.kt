fd173a2d2473bc675ba7267beb438e4d468a3d5e
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2024-12-22 19:50:38 +0100

    Fix aliases test

--- src/commonTest/kotlin/teksturepako/pakku/api/projects/ProjectTest.kt
+++ src/commonTest/kotlin/teksturepako/pakku/api/projects/ProjectTest.kt
@@ -1,36 +1,37 @@
 package teksturepako.pakku.api.projects
 
-import io.mockk.every
-import io.mockk.mockk
 import kotlin.test.Test
 import kotlin.test.assertTrue
 
 class ProjectTest
 {
     @Test
-    fun hasAliasOf_whenProjectHasAlias_returnsTrue()
+    fun `test aliases`()
     {
-        val project1 = mockk<Project> {
-            every { id } returns mutableMapOf("id1" to "id1")
-            every { name } returns mutableMapOf("name1" to "name1")
-            every { slug } returns mutableMapOf("slug1" to "slug1")
-            every { aliases } returns mutableSetOf("id3")
-            every { hasAliasOf(any()) } answers { callOriginal() }
-        }
-        val project2 = mockk<Project> {
-            every { id } returns mutableMapOf("id2" to "id2")
-            every { name } returns mutableMapOf("name2" to "name2")
-            every { slug } returns mutableMapOf("slug2" to "slug2")
-            every { aliases } returns mutableSetOf("name1")
-            every { hasAliasOf(any()) } answers { callOriginal() }
-        }
-        val project3 = mockk<Project> {
-            every { id } returns mutableMapOf("id3" to "id3")
-            every { name } returns mutableMapOf("name3" to "name3")
-            every { slug } returns mutableMapOf("slug3" to "slug3")
-            every { aliases } returns mutableSetOf("slug2")
-            every { hasAliasOf(any()) } answers { callOriginal() }
-        }
+        val project1 = Project(
+            type = ProjectType.MOD,
+            id = mutableMapOf("id1" to "id1"),
+            name = mutableMapOf("name1" to "name1"),
+            slug = mutableMapOf("slug1" to "slug1"),
+            aliases = mutableSetOf("id3"),
+            files = mutableSetOf()
+        )
+        val project2 = Project(
+            type = ProjectType.MOD,
+            id = mutableMapOf("id2" to "id2"),
+            name = mutableMapOf("name2" to "name2"),
+            slug = mutableMapOf("slug2" to "slug2"),
+            aliases = mutableSetOf("name1"),
+            files = mutableSetOf()
+        )
+        val project3 = Project(
+            type = ProjectType.MOD,
+            id = mutableMapOf("id3" to "id3"),
+            name = mutableMapOf("name3" to "name3"),
+            slug = mutableMapOf("slug3" to "slug3"),
+            aliases = mutableSetOf("slug2"),
+            files = mutableSetOf()
+        )
 
         assertTrue(project1 hasAliasOf project3)
         assertTrue(project2 hasAliasOf project1)
