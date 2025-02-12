fa4e9e77d8b1a30cfb191bf02e563d8b68a82c37
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2025-02-12 10:46:42 +0100

    Refactor action error messages

--- src/commonMain/kotlin/teksturepako/pakku/api/actions/errors/ActionError.kt
+++ src/commonMain/kotlin/teksturepako/pakku/api/actions/errors/ActionError.kt
@@ -9,5 +9,7 @@ abstract class ActionError
     /** A message dedicated for the CLI. It should not be used outside of terminal. */
     open fun message(arg: String = ""): String = rawMessage
 
-    protected fun message(vararg args: String): String = args.joinToString("")
+    protected fun message(vararg args: Any?, newline: Boolean = false): String =
+        if (newline) args.joinToString("\n") { it.toString() }
+        else args.joinToString("") { if (it !is String) it.toString() + " " else it }
 }
--- src/commonMain/kotlin/teksturepako/pakku/api/actions/errors/ActionErrors.kt
+++ src/commonMain/kotlin/teksturepako/pakku/api/actions/errors/ActionErrors.kt
@@ -52,10 +52,12 @@ data class NoHashes(val path: Path?) : ActionError()
 
 data class HashMismatch(val path: Path?, val originalHash: String, val newHash: String) : ActionError()
 {
-    override val rawMessage = """Failed to math hash for file '$path'.
-            | Original hash: $originalHash
-            | New hash: $newHash
-            """.trimMargin()
+    override val rawMessage = message(
+        "Failed to math hash for file '$path'.",
+        "Original hash: $originalHash",
+        "New hash: $newHash",
+        newline = true,
+    )
 }
 
 data class CouldNotSave(val path: Path?, val reason: String? = "") : ActionError()
@@ -89,25 +91,32 @@ data class ProjNotFound(val projectInput: String? = null, val project: Project? 
 
 data class ProjDiffTypes(val project: Project, val otherProject: Project) : ActionError()
 {
-    override val rawMessage = """Can not combine two projects of different types:
-            | ${project.slug} ${project.type} + ${otherProject.slug} ${otherProject.type}
-            """.trimMargin()
+    override val rawMessage = message(
+        "Can not combine two projects of different types:",
+        "${project.slug} ${project.type} + ${otherProject.slug} ${otherProject.type}",
+        newline = true
+    )
 
-    override fun message(arg: String): String = """Can not combine two projects of different types:
-            | ${project.getFlavoredSlug()} ${dim(project.type)} + ${otherProject.getFlavoredSlug()} ${dim(otherProject.type)}
-            """.trimMargin()
+    override fun message(arg: String): String = message(
+        "Can not combine two projects of different types:",
+        "${project.getFlavoredSlug()} ${dim(project.type)} + ${otherProject.getFlavoredSlug()} ${dim(otherProject.type)}",
+        newline = true
+    )
 }
 
 data class ProjDiffPLinks(val project: Project, val otherProject: Project) : ActionError()
 {
-    override val rawMessage = """Can not combine two projects with different pakku links:
-            | ${project.slug} ${project.type} + ${otherProject.slug} ${otherProject.type}
-            """.trimMargin()
+    override val rawMessage = message(
+        "Can not combine two projects with different pakku links:",
+        "${project.slug} ${project.type} + ${otherProject.slug} ${otherProject.type}",
+        newline = true
+    )
 
-    override fun message(arg: String): String = """Can not combine two projects with different pakku links:
-                | ${project.getFlavoredSlug()} ${dim(project.type)} +
-                | ${otherProject.getFlavoredSlug()} ${dim(otherProject.type)}
-                """.trimMargin()
+    override fun message(arg: String): String = message(
+        "Can not combine two projects with different pakku links:",
+        "${project.getFlavoredSlug()} ${dim(project.type)} + ${otherProject.getFlavoredSlug()} ${dim(otherProject.type)}",
+        newline = true
+    )
 }
 
 // -- EXPORT --
@@ -186,37 +195,46 @@ data class NoFilesOn(val project: Project, val provider: Provider) : ActionError
 
 data class NoFiles(val project: Project, val lockFile: LockFile) : ActionError()
 {
-    override val rawMessage = """No files found for ${project.type} ${project.slug}.
-            | Your modpack requires Minecraft versions: ${lockFile.getMcVersions()} and loaders: ${lockFile.getLoaders()}.
-            | Make sure the project complies these requirements.
-            """.trimMargin()
+    override val rawMessage = message(
+        "No files found for ${project.type} ${project.slug}.",
+        "Your modpack requires Minecraft versions: ${lockFile.getMcVersions()} and loaders: ${lockFile.getLoaders()}.",
+        "Make sure the project complies these requirements.",
+        newline = true
+    )
 
-    override fun message(arg: String) = """No files found for ${dim(project.type)} ${project.getFlavoredSlug()}.
-            | Your modpack requires Minecraft versions: ${lockFile.getMcVersions()} and loaders: ${lockFile.getLoaders()}.
-            | Make sure the project complies these requirements.
-            """.trimMargin()
+    override fun message(arg: String) = message(
+        "No files found for ${dim(project.type)} ${project.getFlavoredSlug()}.",
+        "Your modpack requires Minecraft versions: ${lockFile.getMcVersions()} and loaders: ${lockFile.getLoaders()}.",
+        "Make sure the project complies these requirements.",
+        newline = true
+    )
 }
 
 data class FileNamesDoNotMatch(val project: Project) : ActionError()
 {
-    override val rawMessage = """${project.type} ${project.slug} versions do not match across platforms.
-            | ${project.files.map { "${it.type}: ${it.fileName}" }}
-            """.trimMargin()
+    override val rawMessage = message(
+        project.type, project.slug, "versions do not match across platforms.",
+        project.files.map { "${it.type}: ${it.fileName}" }.toString(),
+        newline = true
+    )
 
-    override fun message(arg: String) =
-        """${dim(project.type)} ${project.getFlavoredSlug()} versions do not match across platforms.
-                | ${project.files.map { "${it.type}: ${it.fileName}" }}
-                """.trimMargin()
+    override fun message(arg: String) = message(
+        "${dim(project.type)} ${project.getFlavoredSlug()} versions do not match across platforms.",
+        project.files.map { "${it.type}: ${it.fileName}" }.toString(),
+        newline = true
+    )
 }
 
 // -- REMOVAL --
 
 data class ProjRequiredBy(val project: Project, val dependants: List<Project>) : ActionError()
 {
-    override val rawMessage = "${project.type} ${project.slug} is required by ${dependants.map { it.slug }}"
+    override val rawMessage = message(project.type, project.slug, "is required by ${dependants.map { it.slug }}")
+
+    override fun message(arg: String) = message(
+        "${dim(project.type)} ${project.getFlavoredSlug()} is required by ",
+        dependants.map { it.getFlavoredSlug() }.toMsg(), "."
+    )
 
     override val severity = ErrorSeverity.WARNING
-
-    override fun message(arg: String) = "${dim(project.type)} ${project.getFlavoredSlug()} is required by " +
-                "${dependants.map { it.getFlavoredSlug() }.toMsg()}."
 }
