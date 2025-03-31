d040a510a63324cf03a5df4ecb0a0cee7e8ea90f
juraj-hrivnak <bass.jurajhrivnak@gmail.com>
2025-04-01 00:16:41 +0200

    Implement more tests for Glob pattern matching

--- src/commonTest/kotlin/teksturepako/pakku/io/GlobTest.kt
+++ src/commonTest/kotlin/teksturepako/pakku/io/GlobTest.kt
@@ -10,37 +10,85 @@ import kotlin.io.path.Path
 import kotlin.io.path.pathString
 import kotlin.test.Test
 
-class GlobTest : PakkuTest()
+class GlobTest : PakkuTest(teardown = true)
 {
-    private val testFileName = "test_file.txt"
-    private val testDirName = "test_dir"
+    @Test
+    fun `test glob of single file`(): Unit = runBlocking {
+        val file = "test_file.txt"
+        createTestFile(file)
 
-    override suspend fun `set-up`()
-    {
-        createTestFile(testFileName)
-        createTestDir(testDirName)
-        createTestFile(testDirName, testFileName)
+        val expandedGlob = listOf(file).expandWithGlob(Path(workingPath))
+
+        expectThat(expandedGlob).contains(file)
     }
 
     @Test
-    fun `test with basic file name`(): Unit = runBlocking {
-        val expandedGlob = listOf(testFileName).expandWithGlob(Path(workingPath))
+    fun `test negating glob of single file`(): Unit = runBlocking {
+        val file = "test_file.txt"
+        createTestFile(file)
 
-        expectThat(expandedGlob).contains(testFileName)
+        val expandedGlob = listOf("!$file").expandWithGlob(Path(workingPath))
+
+        expectThat(expandedGlob).doesNotContain(file)
     }
 
     @Test
-    fun `test with negating pattern`(): Unit = runBlocking {
-        val expandedGlob = listOf("!$testFileName").expandWithGlob(Path(workingPath))
+    fun `test all dir content glob with one file negated`(): Unit = runBlocking {
+        val dir = "test_dir"
+        createTestDir(dir)
 
-        expectThat(expandedGlob).doesNotContain(testFileName)
+        val includedFile = "included_file.txt"
+        createTestFile(dir, includedFile)
+
+        val excludedFile = "excluded_file.txt"
+        createTestFile(dir, excludedFile)
+
+        val expandedGlob = listOf(
+            "$dir/**",
+            "!$dir/$excludedFile"
+        ).expandWithGlob(Path(workingPath))
+
+        expectThat(expandedGlob)
+            .contains(Path(dir, includedFile).pathString)
+
+        expectThat(expandedGlob)
+            .doesNotContain(Path(dir, excludedFile).pathString)
     }
 
     @Test
-    fun `test with dir pattern`(): Unit = runBlocking {
-        val expandedGlob = listOf("$testDirName/**").expandWithGlob(Path(workingPath))
+    fun `test negating nested dirs`(): Unit = runBlocking {
+        val dir = "test_dir"
+        createTestDir(dir)
 
-        expectThat(expandedGlob).doesNotContain(testDirName)
-        expectThat(expandedGlob).contains(Path(testDirName, testFileName).pathString)
+        val subDir = "sub_dir"
+        createTestDir(dir, subDir)
+
+        val includedFile = "included_file.txt"
+        createTestFile(dir, includedFile)
+
+        val excludedFile = "excluded_file.txt"
+        createTestFile(dir, excludedFile)
+
+        val excludedFileInSubDir = "excluded_file_2.txt"
+        createTestFile(dir, subDir, excludedFile)
+
+        val expandedGlob = listOf(
+            "$dir/**",
+            "!$dir/$excludedFile",
+            "!$dir/$subDir",
+            "!$dir/$subDir/**"
+        ).expandWithGlob(Path(workingPath))
+
+        expectThat(expandedGlob)
+            .doesNotContain(Path(dir, subDir).pathString)
+
+        expectThat(expandedGlob)
+            .contains(Path(dir, includedFile).pathString)
+
+        expectThat(expandedGlob)
+            .doesNotContain(Path(dir, excludedFile).pathString)
+
+        expectThat(expandedGlob)
+            .doesNotContain(Path(dir, subDir, excludedFileInSubDir).pathString)
     }
 }
