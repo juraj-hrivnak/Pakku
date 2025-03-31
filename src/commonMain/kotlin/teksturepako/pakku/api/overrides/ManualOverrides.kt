b1e927edbdeec7cb18097b540e8204cf209f7f87
juraj-hrivnak <bass.jurajhrivnak@gmail.com>
2025-04-01 00:16:22 +0200

    Remove debug prints for manual overrides

--- src/commonMain/kotlin/teksturepako/pakku/api/overrides/ManualOverrides.kt
+++ src/commonMain/kotlin/teksturepako/pakku/api/overrides/ManualOverrides.kt
@@ -6,9 +6,7 @@ import kotlinx.coroutines.coroutineScope
 import teksturepako.pakku.api.data.ConfigFile
 import teksturepako.pakku.api.data.Dirs.PAKKU_DIR
 import teksturepako.pakku.api.data.workingPath
-import teksturepako.pakku.debugIfNotEmpty
 import teksturepako.pakku.io.tryOrNull
-import teksturepako.pakku.toPrettyString
 import java.io.File
 import java.nio.file.Path
 import kotlin.io.path.Path
@@ -45,9 +43,6 @@ suspend fun readManualOverridesFrom(
     }
     .awaitAll()
     .filterNotNull()
-    .debugIfNotEmpty {
-        println("readManualOverrides = ${it.toPrettyString()}")
-    }
     .toSet()
 
 @Suppress("unused")
