deb228277bf804583ff20d8139de64df97f3d9ba
Juraj Hrivnák <bass.jurajhrivnak@gmail.com>
2025-06-17 21:14:52 +0200

    Fix compilation error

--- src/commonMain/kotlin/teksturepako/pakku/api/actions/sync/SyncOverrides.kt
+++ src/commonMain/kotlin/teksturepako/pakku/api/actions/sync/SyncOverrides.kt
@@ -8,31 +8,35 @@ import kotlinx.coroutines.launch
 import teksturepako.pakku.api.actions.errors.ActionError
 import teksturepako.pakku.api.actions.errors.AlreadyExists
 import teksturepako.pakku.api.overrides.ManualOverride
+import teksturepako.pakku.io.tryOrNull
 import teksturepako.pakku.io.tryToResult
+import kotlin.io.path.copyTo
 import kotlin.io.path.createParentDirectories
 import kotlin.io.path.exists
-import kotlin.io.path.writeBytes
 
 suspend fun Set<ManualOverride>.sync(
     onError: suspend (error: ActionError) -> Unit,
     onSuccess: suspend (manualOverride: ManualOverride) -> Unit,
     syncPrimaryDirectories: Boolean = false,
 ): List<Job> = coroutineScope {
-    this@sync.mapNotNull { projectOverride ->
-        if (projectOverride.isInPrimaryDirectory && !syncPrimaryDirectories) return@mapNotNull null
+    this@sync.mapNotNull { manualOverride ->
+        if (manualOverride.isInPrimaryDirectory && !syncPrimaryDirectories) return@mapNotNull null
 
         launch {
-            if (projectOverride.fullOutputPath.exists())
+            if (manualOverride.fullOutputPath.exists())
             {
-                onError(AlreadyExists(projectOverride.fullOutputPath.toString()))
+                onError(AlreadyExists(manualOverride.fullOutputPath.toString()))
                 return@launch
             }
 
-            projectOverride.fullOutputPath.tryToResult {
+            manualOverride.fullOutputPath.tryOrNull {
                 createParentDirectories()
-                writeBytes(projectOverride.bytes)
+            }
+
+            manualOverride.path.tryToResult {
+                copyTo(manualOverride.fullOutputPath)
             }.onSuccess {
-                onSuccess(projectOverride)
+                onSuccess(manualOverride)
             }.onFailure {
                 onError(it)
             }
